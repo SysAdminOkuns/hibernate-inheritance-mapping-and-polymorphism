0d882f5691de3cad50dc68952049b3ae2bd360da
SysAdminOkuns <sysad.okundaye@gmail.com>
2023-04-26 17:43:47 -0400

    hibernate with inheritance mapping and polymorphism queries

---
+++ src/main/java/entity/Dog.java
@@ -0,0 +1,36 @@
+
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class Dog extends Animal {
+	
+	//Used in Question1Client
+	/*
+	@Column(name="barking_level")
+	private Integer barkingLevel;
+	public void setBarkingLevel(Integer barkingLevel) {
+		this.barkingLevel = barkingLevel;
+	}
+	*/
+
+	@Override
+	public String makeNoise() {
+		return "woof woof..";
+	}
+
+    public void setBarkingLevel(int i) {
+    }
+}
+
+
+
+
+
+
+
+
+
+
